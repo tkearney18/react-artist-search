fdbd026450b50fc7c6dcb012fd1b6f5da97aaf84
Troy Kearney <tkearney18@gmail.com>
2017-03-29 13:29:33 -0400

    Updated the index.js with the App component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// This is where the root of the application will live
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Search from './components/Search'
+
+class App extends React.Component {
+
+    handleSearchChange(term) {
+        console.log(term)
+    }
+
+    render() {
+        return(
+            <div className="greeting">
+                <Search onSearchChange={ this.handleSearchChange } />
+            </div>
+
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
