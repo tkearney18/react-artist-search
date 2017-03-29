3dc6142bae0d8de7d0f325cef5329b4dc09b8ac1
Troy Kearney <tkearney18@gmail.com>
2017-03-29 18:22:32 -0400

    Fixed the inline style and changed the css classes.

--- src/components/Search.js
+++ src/components/Search.js
@@ -17,10 +17,10 @@ class Search extends React.Component {
     }
     render() {
         return(
-            <div className="search col-lg-6 col-lg-push-4" >
+            <div className="search col-lg-6 col-lg-push-3" >
                 <form onSubmit={event => this.onTermSubmission(event)}>
-                    <span style={{ width: 80% }}>Search: </span>
-                    <input onChange={event => this.onTermChange(event.target.value)} />
+                    <span>Search: </span>
+                    <input onChange={event => this.onTermChange(event.target.value)} style={{ width: 400 }} />
                 </form>
             </div>
 
