aaafee5b11fc74b8b685f1d8342a22d48cf86e40
Troy Kearney <tkearney18@gmail.com>
2017-03-30 12:43:07 -0400

    Updated the artist component with the data returned from spotify

--- src/components/Artist.js
+++ src/components/Artist.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Artist = ({ artist, classes }) => {
+const Artist = ({ artist }) => {
     return(
-        <div className={ classes }>
-            <a href={ artist.link }> <h2>{ artist.name }</h2></a>
-                <img src={ artist.image } />
+        <div>
+            <a href={ artist.href }> <h2>{ artist.name }</h2></a>
+                {!artist.images.length ? <p>No Image Available</p> : <img src={ artist.images[0].url } style={{ 
+                    height: 100, width: 100
+                }} />}
                 <ul>
                     {artist.genres.map(g =>
                         <li key={Math.random()}>{g}</li>
