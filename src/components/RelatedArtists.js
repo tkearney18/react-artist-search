6ee2b7839bc5afeca1828f4dcddd13103d2208a5
Troy Kearney <tkearney18@gmail.com>
2017-03-31 16:19:36 -0400

    Added functionality for using rows with lodash and bootstrap

--- src/components/RelatedArtists.js
+++ src/components/RelatedArtists.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Artist from './Artist'
+import _ from 'lodash'
 
 const RelatedArtists = ({ relatedArtists }) => {
+    let groups = _.chunk(relatedArtists, 4);
     return(
         <div>
-            {relatedArtists.map( artist =>
-                <Artist key={Math.random()} artist={ artist }/>)
-            }
+            {groups.map(renderRow)}
         </div>
     )
+    function renderRow(row) {
+        return(
+            <div className="row">
+                { row.map(renderItem) } 
+            </div>
+        )
+    }
+    function renderItem(item) {
+        return(
+            <div className="col-sm-3">
+                <Artist artist={item} />
+            </div>
+        )
+    }
 }
 
+            // {relatedArtists.map( artist =>
+            //     <Artist key={Math.random()} artist={ artist }/>)
+            // }
 export default RelatedArtists;
